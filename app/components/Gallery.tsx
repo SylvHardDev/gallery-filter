04b9dd40012b2665ba18e337d6fe0462801756e7
SylvHardDev <hardsyldev@gmail.com>
2024-12-23 13:42:05 +0300

    structure

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -7,7 +7,12 @@ export default function Gallery() {
     <section className="pt-20 px-4 sm:px-6 lg:px-8 bg-gray-900 min-h-screen">
       <h1 className="uppercase text-6xl font-black text-center text-white mb-8">
         Filter <span className="text-pink-500">galery</span>
+
+        <div className="max-w-[1000px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+
+        </div>
       </h1>
     </section>
   );
 }
+ 
