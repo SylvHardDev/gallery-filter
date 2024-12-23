954aa5194b79bb0e3faf970508ca2b48a8c0b663
SylvHardDev <hardsyldev@gmail.com>
2024-12-23 13:35:59 +0300

    title gallery

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 
 export default function Gallery() {
   return (
-    <div>Gallery</div>
-  )
+    <section className="pt-20 px-4 sm:px-6 lg:px-8 bg-gray-900 min-h-screen">
+      <h1 className="uppercase text-6xl font-black text-center text-white mb-8">
+        Filter <span className="text-pink-500">galery</span>
+      </h1>
+    </section>
+  );
 }
